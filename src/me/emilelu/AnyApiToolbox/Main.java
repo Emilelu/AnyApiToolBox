f4cfe50be240378387c421d7bd60fdf00e459c38
Lu <kimiuso@vip.qq.com>
2020-08-04 14:14:31 +0800

    Change: Minimum Java version to 8

--- src/me/emilelu/AnyApiToolbox/Main.java
+++ src/me/emilelu/AnyApiToolbox/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("欢迎使用 AnyApi 工具箱 [Version 0.1.5-beta]");
+		System.out.println("欢迎使用 AnyApi 工具箱 [Version 0.1.6]");
 		System.out.println("目前，本软件接入了 小歪 和 搏天 两大 API，使用到了各 API 的部分功能。");
 		System.out.println("本软件的核心功能为「批量」，包括但不限于批量下载、批量输出+保存。");
 		System.out.println("当然，您也可以自行输入一个 URL 来使用功能。");
